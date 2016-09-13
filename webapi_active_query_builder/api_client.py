62912da606059332ff2f867c11a4c9591c7a5271
ActiveDbSoft <development@activedbsoft.com>
2016-09-13 15:03:41 +0500

    disable html escaping in docs

--- webapi_active_query_builder/api_client.py
+++ webapi_active_query_builder/api_client.py
@@ -89,7 +89,7 @@ class ApiClient(object):
             self.host = host
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'Swagger-Codegen/1.1.3/python'
+        self.user_agent = 'Swagger-Codegen/1.1.6/python'
 
     @property
     def user_agent(self):
